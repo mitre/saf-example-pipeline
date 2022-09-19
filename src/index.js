631f2dce92b3efa9c6f0b1880820c9eda400aa89
Amndeep Singh Mann <amann@mitre.org>
2022-09-19 04:47:29 -0400

    copying from their vulnerable code sections has to work eventually... right?!
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const content = document.getElementById("content");
 	content.textContent = "This text was swapped in programmatically."
-
-	// intentional vulnerability section - unsanitized hash section from the url is being put into the dom
-	const vuln = document.getElementById("vuln");
-	vuln.innerHTML = decodeURIComponent(location.hash.substr(1));
 }, false);
+
+// intentional vulnerability section - origins should be verified during cross-origin communications
+window.addEventListener("message", function(event) {
+	console.log(event.data);
+});
