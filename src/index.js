26d32525a86dc6d860934fd3ae83a32258b8f7fa
Amndeep Singh Mann <amann@mitre.org>
2022-09-19 04:42:11 -0400

    pretty pls sonarqube
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const content = document.getElementById("content");
-	eval(content.textContent); // intentional vulnerability
 	content.textContent = "This text was swapped in programmatically."
+
+	// intentional vulnerability section - unsanitized hash section from the url is being put into the dom
+	const vuln = document.getElementById("vuln");
+	vuln.innerHTML = decodeURIComponent(location.hash.substr(1));
 }, false);
